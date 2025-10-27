65ce6bb9118ef49e99b458aa1a7aeb0af71ae5cf
atiqisrak <atiq.israk@webable.digital>
2025-10-27 21:17:14 +0600

    added page insight vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -123,6 +124,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <WhatsAppButton />
+        <SpeedInsights />
       </body>
     </html>
   );
